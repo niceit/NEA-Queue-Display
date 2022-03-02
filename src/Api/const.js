aabc34a1eabad3c421ce4656e646a77eb0a506e5
Tran IT <tranit1209@gmail.com>
2022-03-02 21:04:23 +0700

    Update API

--- src/Api/const.js
+++ src/Api/const.js
@@ -2,7 +2,7 @@ export const API_PATH = {
     GET_COMPANY_LIST: 'GetCompanyList',
     GET_OUTLET_LIST: (companyId) => `GetOutletList?CompanyId=${companyId}`,
     CALL_ORDER_LIST: (outletId, orderDate) => {
-        return `CallOrderList?outletId=${outletId}&orderDate=${orderDate}`
+        return `CallOrderList?outletId=${outletId}&orderDate=${orderDate}&status=View,Ready,Collected`
     }
 }
 
